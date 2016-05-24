c2efa8d3b3fda3f5ccba218bdbfa57dbd1c3e2d6
Nathan Kot <me@nathankot.com>
2016-05-24 15:36:28 +0900

    Bump to 0.1.1

--- counsel-dash.el
+++ counsel-dash.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2016 Nathan Kot
 
 ;; Author: Nathan Kot <nk@nathankot.com>
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires ((cl-lib "0.5") (dash "2.12.1") (dash-functional "1.2.0") (helm-dash "1.3.0") (counsel "0.8.0"))
 ;; Keywords: dash, ivy-mode, counsel=mode
 ;; URL: https://github.com/nathankot/counsel-dash
